a1459cd9180f2ac11dfed522484eb7a57dfa62d8
jisoooo17 <jisoooo17@gmail.com>
2021-02-23 17:18:24 +0900

    groddd

--- js/header.js
+++ js/header.js
@@ -1,3 +1,10 @@
 $(document).ready(function(){
-    
+    var $skip_a = $("#skipNavi a");
+
+    $skip_a.on("focusin",function(){
+        $(this).css({top:0});
+    });
+    $skip_a.on("focusout",function(){
+        $(this).css({top:-30});
+    })
 })
